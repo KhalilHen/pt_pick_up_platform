455f8361ac1b8391432394dd80b1d7b195a95ea2
Khalil <khalilhenane@hotmail.com>
2025-01-11 22:11:00 +0100

    Added  firebase + supabase + added the intial routes i have in mind

---
+++ lib/routes/routes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Routes {
+  static const String onBoarding = '/onboarding';
+  static const String authGate = '/auth_gate';
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String signUp = '/sign_up';
+   
+
+
+   static Map<String, WidgetBuilder> returnRoutes = {
+    // onBoarding: (context) => OnBoarding(),
+    // authGate: (context) => AuthGate(),
+    // home: (context) => Home(),
+    // login: (context) => Login(),
+    // signUp: (context) => SignUp(),
+};
+
+}
