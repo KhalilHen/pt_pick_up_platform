25ab1a31bb749b30f18ddc67330f8af80eaedec2
Khalil <khalilhenane@hotmail.com>
2025-01-14 20:28:15 +0100

    Worked further on card view but got some problem
    Currently when i try to work on menu items the items get's blank. While i am  not using listview.builder

--- lib/pages/detailed_restaurant_view.dart
+++ lib/pages/detailed_restaurant_view.dart
@@ -37,7 +37,152 @@ SliverAppBar(
 
 
 )     ,  
+  SliverToBoxAdapter(
 
+    child: Padding(padding: const EdgeInsets.all(16),
+    
+    
+    child: Column(
+
+        crossAxisAlignment: CrossAxisAlignment.start,
+
+        children: [
+
+          Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            children: [
+              Row(
+
+                children: [
+       const Icon(Icons.star, color: Colors.amber, size: 20,),
+
+              Text(
+              'Rating: 4.5',
+              style: Theme.of(context).textTheme.headlineMedium,
+
+              ),
+   
+                ],
+              ),
+
+       
+   
+   Text('Reviews',
+   
+   
+                //  style: Theme.of(context).textTheme.headlineMedium,s
+                style: TextStyle(
+                  color: Colors.grey[600]
+                ),
+  )
+            ],
+          ),
+
+               Container(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 12,
+                          vertical: 6,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.deepOrange.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                        child: Row(
+                          children: [
+                            Icon(
+                              Icons.timer,
+                              color: Colors.deepOrange,
+                            ),
+                            Padding(padding: const EdgeInsets.only(right: 8)),
+                            Text(
+                              '30 min',
+                              style: const TextStyle(
+                                color: Colors.deepOrange,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+const SizedBox(height: 16,),
+
+ SingleChildScrollView(
+
+    scrollDirection: Axis.horizontal,
+
+
+    child: Row(
+
+      children: [
+
+        Container(
+//Map here late all category
+          margin: const EdgeInsets.only(right: 8),
+
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+          decoration: BoxDecoration(
+            color: Colors.grey[200],
+            borderRadius: BorderRadius.circular(20),          ),
+
+            child: Text('Category'),
+
+
+
+        ),
+
+        const SizedBox(height: 24,),
+
+
+        Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+
+                children: [
+                  Text(
+                    'section',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+
+                  const SizedBox(height: 16,),
+                  //TODO Change ListView later when retrieving from DB
+                  ListView(
+
+                    children: [
+                      Padding(padding: 
+                      
+                      const EdgeInsets.symmetric(vertical: 8),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Container(
+
+                            width: 80,
+                            height: 80,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                image: NetworkImage('https://www.bartsboekje.com/wp-content/uploads/2020/06/riccardo-bergamini-O2yNzXdqOu0-unsplash-scaled.jpg'),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      )
+                    ],
+                  )
+                ],
+        )
+      ],
+    ),
+ )                  
+                
+        ],
+    ),
+    ),
+
+  ),
 
    ],
           ),
