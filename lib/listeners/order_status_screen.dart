e797939503fa7eed62459691cd67996be431a98b
Khalil <khalilhenane@hotmail.com>
2025-01-19 13:26:39 +0100

    Created bit of the status header
    Created the status header. But i am gonna convert it into a custom widget. To make it easy making the page structure. As it can be much to just put in 1

--- lib/listeners/order_status_screen.dart
+++ lib/listeners/order_status_screen.dart
@@ -6,10 +6,10 @@ import 'package:pt_pick_up_platform/models/enum/order_enum.dart';
 class OrderStatusScreen extends StatefulWidget {
   // final int orderId;
 
-  const OrderStatusScreen({Key? key,
-  //  required this.orderId
-   }
-  ) : super(key: key);
+  const OrderStatusScreen({
+    Key? key,
+    //  required this.orderId
+  }) : super(key: key);
 
   @override
   State<OrderStatusScreen> createState() => _OrderStatusScreenState();
@@ -28,19 +28,40 @@ class _OrderStatusScreenState extends State<OrderStatusScreen> with TickerProvid
         ),
         elevation: 0,
       ),
-
       body: SafeArea(
-        
-        
-        
-        child: Column(
-
-            children: [
-              //Status Header
-              Container()
-            ],
-
-        )),
+          child: Column(
+        children: [
+          //Status Header
+          Container(
+            padding: const EdgeInsets.all(24),
+            decoration: BoxDecoration(color: Colors.white, boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withAlpha(22),
+                spreadRadius: 1,
+                blurRadius: 10,
+              )
+            ]),
+            child: Column(
+              children: [
+                //
+                Icon(
+                  Icons.check_circle,
+                  color: Colors.green,
+                  size: 48,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  'Order Confirmed',
+                  style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.deepOrange),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
