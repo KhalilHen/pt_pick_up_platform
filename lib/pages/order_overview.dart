eec4eca1872e856bd6523c246e3c4c9faae8744a
Khalil <khalilhenane@hotmail.com>
2025-01-21 07:51:11 +0100

    Created  bit of the structure of the order history

--- lib/pages/order_overview.dart
+++ lib/pages/order_overview.dart
@@ -3,10 +3,23 @@ import 'package:flutter/material.dart';
 class OrdersPage extends StatelessWidget {
   OrdersPage({Key? key}) : super(key: key);
 
-  final orders = [];
+  final orders = [
+    {
+      {
+        'id': 1,
+        'status': 'Completed',
+        'date': '20-1-2025',
+        'restaurantName': 'Restaurant A',
+        'items': 5,
+        'total': 20.0,
+        'image': 'https://via.placeholder.com/50',
+      },
+    }
+  ];
 
   @override
   Widget build(BuildContext context) {
+    //TODO Improve the structure  
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -45,9 +58,135 @@ class OrdersPage extends StatelessWidget {
                 ),
               )
             : ListView.builder(
+                padding: const EdgeInsets.all(16),
                 itemCount: orders.length,
                 itemBuilder: (context, index) {
-                  return ListTile();
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withAlpha((0.1 * 255).round()),
+                          spreadRadius: 1,
+                          blurRadius: 10,
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            // color: Colors.
+                            borderRadius: const BorderRadius.vertical(top: Radius.circular(12)),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(
+                                Icons.done_all,
+                                color: Colors.green,
+                                size: 24,
+                              ),
+                              const SizedBox(
+                                width: 8,
+                              ),
+                              Text(
+                                'Completed',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        // Order date
+                        Text(
+                          '20-1-2025',
+                          style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  Container(
+                                    width: 50,
+                                    height: 50,
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(8),
+                                      child: Image.network(
+                                        'image',
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    width: 12,
+                                  ),
+                                  Expanded(
+                                      child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        'restaurant name',
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        '5  items   • \€20',
+                                        style: TextStyle(
+                                          color: Colors.grey[600],
+                                        ),
+                                      ),
+                                    ],
+                                  ))
+                                ],
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              Text(
+                                'Order# 51',
+                                style: TextStyle(
+                                  color: Colors.grey[600],
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        InkWell(
+                          onTap: null,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(vertical: 12),
+                            decoration: BoxDecoration(border: Border(top: BorderSide(color: Colors.grey[200]!))),
+                            child: const Center(
+                              child: Text(
+                                'View order details',
+                                style: TextStyle(
+                                  color: Colors.deepOrange,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
                 },
               ),
       ),
